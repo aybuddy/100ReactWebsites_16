397f5d6e62ed71b0d630d4fa2264abb269f4d23f
Aaron Yu <ayybuddyy@gmail.com>
2021-03-07 17:29:20 -0500

    Create and style Banner component

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Banner from './Banner';
 import './HomeScreen.css';
 import Nav from './Nav';
 
@@ -6,8 +7,7 @@ const HomeScreen = () => {
   return (
     <div className='homeScreen'>
       <Nav />
-      {/* <Banner /> */}
-
+      <Banner />
       {/* <Row /> */}
     </div>
   );
