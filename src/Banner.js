930741626429f9e97f3dccb02a3dad11301bc2e1
Aaron Yu <ayybuddyy@gmail.com>
2021-03-07 18:18:46 -0500

    Install axios, connect app to api and get requests

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import axios from './axios';
+import React, { useEffect, useState } from 'react';
 import './Banner.css';
+import requests from './Requests';
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   const truncate = (string, n) => {
     return string?.length > n ? string.substr(0, n - 1) + '...' : string;
   };
@@ -12,20 +32,19 @@ const Banner = () => {
       style={{
         backgroundPosition: 'center center',
         backgroundSize: 'cover',
-        backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
       }}
     >
       <div className='banner__contents'>
-        <h1 className='banner__title'>Movie Name</h1>
+        <h1 className='banner__title'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className='banner__buttons'>
           <button className='banner__button'>Play</button>
           <button className='banner__button'>My List</button>
         </div>
         <h1 className='banner__description'>
-          {truncate(
-            `This is a test description for a Netflix Clone. This is a test description for a Netflix Clone. This is a test description for a Netflix Clone. This is a test description for a Netflix Clone. This is a test description for a Netflix Clone.`,
-            150
-          )}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
       <div className='banner--fadeBottom' />
