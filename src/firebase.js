5c751215b542599732523ff495927e5f2e08d87f
Aaron Yu <ayybuddyy@gmail.com>
2021-03-07 13:46:08 -0500

    Replace files with redux template

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyD4zlVNF9zuScQKyjo1AehmGHrWj5s6bMM',
+  authDomain: 'netflix-build-ss.firebaseapp.com',
+  projectId: 'netflix-build-ss',
+  storageBucket: 'netflix-build-ss.appspot.com',
+  messagingSenderId: '977576782768',
+  appId: '1:977576782768:web:9cb44b29930d13a8cb0475',
+  measurementId: 'G-WHW6KRZ6NF',
+};
