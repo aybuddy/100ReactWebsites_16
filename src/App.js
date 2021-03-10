c2952ed6be1f2635558809e45542df3f309b9b6f
Aaron Yu <ayybuddyy@gmail.com>
2021-03-10 18:07:45 -0500

    Create userSlice using Redux and connect it to store

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         // logged in
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
         // logged out
+        dispatch(logout);
       }
     });
     return unsubscribe;
